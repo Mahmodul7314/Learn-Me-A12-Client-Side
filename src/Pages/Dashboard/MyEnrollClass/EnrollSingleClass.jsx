d3e0b130e0443d63036dc3e0f6d93b33a8c11951
Mahmodul7314 <mahmudulhasan7314@gmail.com>
2024-01-01 04:49:56 +0600

    enroll my class complete

--- src/Pages/Dashboard/MyEnrollClass/EnrollSingleClass.jsx
+++ src/Pages/Dashboard/MyEnrollClass/EnrollSingleClass.jsx
@@ -1,9 +1,32 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 
-const EnrollSingleClass = () => {
+const EnrollSingleClass = ({course}) => {
+
+     const {image,title,name,email,description,price,enroll,reviews} = course;
     return (
         <div>
-            
+               <div>
+            <div className="card bg-base-100 shadow-xl lg:px-0 md:px-0 px-6">
+            <figure><img src={image} alt="Shoes" /></figure>
+            <div className="card-body h-[26rem]">
+                <h2 className="card-title">{title}</h2>
+               <div>
+               <p>{name}</p>
+                <p>{email}</p>
+                </div> 
+                <div className="text-sm font-normal text-justify space-y-2"> Description: {description}</div>
+                <div className="text-yellow-500 font-bold">Price:{price} $</div>
+                <div className="flex justify-around">
+                    <p className="text-gray-600 font-bold">Enroll: {enroll}</p>
+                    <p className="text-gray-600 font-bold">Reviews: {reviews}</p>
+                </div>
+                <div className="card-actions justify-center pt-8">
+                {/* <button className="btn btn-warning">Enroll Now</button> */}
+                </div>
+            </div>
+        </div>
+        </div>
         </div>
     );
 };
--- src/Pages/Dashboard/MyEnrollClass/MyEnrollClass.jsx
+++ src/Pages/Dashboard/MyEnrollClass/MyEnrollClass.jsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
 import useAxiosSecure from "../../../Hooks/useAxiosSecure";
 import { useQuery } from "@tanstack/react-query";
+import EnrollSingleClass from "./EnrollSingleClass";
+
 
 
 
@@ -22,7 +24,12 @@ const MyEnrollClass = () => {
         <div>
             <h3 className="text-5xl">My Enroll class</h3>
             <div>{classes.length}</div>
-
+            <div className='grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-4 lg:gap-y-0 md:gap-y-3 gap-y-4'>
+        {
+            classes.map(course=><EnrollSingleClass key={course._id} course={course}
+            ></EnrollSingleClass>)
+        }
+        </div>
             <div className="flex justify-center py-40"><Link className="btn btn-warning" to="/">GO Home</Link></div> 
         </div>
     );
