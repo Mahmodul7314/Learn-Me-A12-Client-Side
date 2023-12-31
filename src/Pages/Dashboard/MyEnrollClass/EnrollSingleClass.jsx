c916291804c8cfa856390c310ea85cbccfeb9c14
Mahmodul7314 <mahmudulhasan7314@gmail.com>
2024-01-01 04:35:34 +0600

    myenroll code

---
+++ src/Pages/Dashboard/MyEnrollClass/EnrollSingleClass.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const EnrollSingleClass = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default EnrollSingleClass;
--- src/Pages/Dashboard/MyEnrollClass/MyEnrollClass.jsx
+++ src/Pages/Dashboard/MyEnrollClass/MyEnrollClass.jsx
@@ -1,14 +1,27 @@
 import { Link } from "react-router-dom";
+import useAxiosSecure from "../../../Hooks/useAxiosSecure";
+import { useQuery } from "@tanstack/react-query";
+
+
 
 
 const MyEnrollClass = () => {
+
+   
+        const axiosSecure = useAxiosSecure();
+        const {data: classes = [], refetch} = useQuery({
+            queryKey: ['classes'],
+            queryFn: async() =>{
+                const res = await axiosSecure.get('/enrollclass');
+                return res.data;
+            }
+        })
+    
+     
     return (
         <div>
             <h3 className="text-5xl">My Enroll class</h3>
-
-
-
-
+            <div>{classes.length}</div>
 
             <div className="flex justify-center py-40"><Link className="btn btn-warning" to="/">GO Home</Link></div> 
         </div>
